0977f93a428d8d14822dff879e539e762cfe1ae6
Elie AMAR <eamar@bess-f4r1s3.clusters.42paris.fr>
2023-10-05 19:31:15 +0200

    NESTJS utilisation de @Body dans auth controller pour recuperer le contenu du POST de insomnia

--- requirements/nestjs/app/src/auth/auth.controller.ts
+++ requirements/nestjs/app/src/auth/auth.controller.ts
@@ -1,21 +1,28 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Req, Body } from "@nestjs/common";
 
 import { AuthService } from "./auth.service";
+
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController{
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
-    //return 'I AM SIGNED UPPP';
+  signup(@Body() dto: any) {
+  //signup(@Req() req: Request) {
+    //console.log(req);
+    console.log( {dto} );
+
     return this.authService.signup();
+    //return 'I AM SIGNED UPPP';
   }
 
   @Post('signin')
   signin() {
-    //return 'I AM SIGNED INNN';
+
     return this.authService.signin();
+    //return 'I AM SIGNED INNN';
   }
 
   @Post('testobject')
