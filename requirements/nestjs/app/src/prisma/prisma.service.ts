75156cd2bbb09584dc4a738f903a19d5e2e26a18
Elie AMAR <eamar@paul-f4Br8s3.clusters.42paris.fr>
2023-10-08 18:27:55 +0200

    prisma service: retrait de l'url en dur et utilisation de config..get

--- requirements/nestjs/app/src/prisma/prisma.service.ts
+++ requirements/nestjs/app/src/prisma/prisma.service.ts
@@ -6,17 +6,15 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor(config: ConfigService) {
-    super({
-      datasources: {
-        db: {
-		url: 'postgresql://myuser:mypass@postgres_container:5432/mydb?schema=public'
-	},
+	constructor(config: ConfigService) {
+		super({
+			datasources: {
+				db: {
+					url: config.get('DATABASE_URL')
+				},
 
-      },
-    });
-	  //console.log(config.get('DATABASE_URL'));
-	  //url: config.get('DATABASE_URL'),
-  }
+			},
+		});
+	}
 }
 //url: 'postgresql://myuser:mypass@postgres_container:5432/mydb?schema=public'
