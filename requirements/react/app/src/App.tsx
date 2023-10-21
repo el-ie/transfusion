284b4049e1f507278988ff075bd2686c11f9a84c
Elie AMAR <eamar@made-f0Br3s4.clusters.42paris.fr>
2023-10-22 01:25:20 +0200

    REACT: creation de LoginForm, pour l'instant on recupere les informations du rpompt comme il faut, maintenant on va voir comment les transferer ailleur

--- requirements/react/app/src/App.tsx
+++ requirements/react/app/src/App.tsx
@@ -2,25 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import LoginForm from './components/LoginForm/LoginForm';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	return (
+			<div className="App">
+			<p> coucou </p><br/><br/><br/><br/>
+			<LoginForm/>
+			</div>
+		   );
 }
+
 
 export default App;
---
+++ requirements/react/app/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: String,
+      password: String,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+	  if (event.target.name === "log_field")
+		  this.setState({ login: event.target.value });
+	  else
+		  this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('login = ' + this.state.login + ' pass = ' + this.state.password);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Login: <br/>
+          <input type="text" name="log_field" value={this.state.login} onChange={this.handleInputChange} />
+        </label>
+        <br /><br />
+        <label>
+          Password: <br/>
+          <input type="text" name="pass_field" value={this.state.password} onChange={this.handleInputChange} />
+        </label>
+		<br /><br />
+		<input type="submit" value="      Login      "/>
+      </form>
+    );
+  }
+}
