872e9db9a14ae72880d4da3a8b36c64c08871932
Elie AMAR <eamar@made-f0Br2s7.clusters.42paris.fr>
2023-10-22 17:02:04 +0200

    NESTJS: enable cors dans le main

--- requirements/nestjs/app/src/main.ts
+++ requirements/nestjs/app/src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe( { whitelist: true }));
 	app.use(cookieParser());
+	app.enableCors();
 	//app.use(passport.initialize());
 	await app.listen(3001);
 }
