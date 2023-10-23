e489eddd375d48b3dcd55c22c1141173b20ecae9
Elie AMAR <eamar@made-f0Br7s17.clusters.42paris.fr>
2023-10-23 23:37:23 +0200

    ++ nest

--- requirements/nestjs/app/src/app.module.ts
+++ requirements/nestjs/app/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RequestModule } from './request/request.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, UserModule, BookmarkModule, PrismaModule],
+	imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, UserModule, PrismaModule, RequestModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
