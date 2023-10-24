9c02b8c8cded7b0b4ff76b609e39ddffac7f47ba
Elie AMAR <eamar@made-f0Br4s2.clusters.42paris.fr>
2023-10-24 23:32:32 +0200

    [NEST] user.service file

---
+++ requirements/nestjs/app/src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+	constructor(private prisma: PrismaService) {}
+
+	async findOneById(id: number) {
+		return this.prisma.user.findUnique({ where: { id: id } });
+	}
+
+	async findOneByUsername(username: string) {
+		return this.prisma.user.findUnique({ where: { username: username } });
+	}
+}
