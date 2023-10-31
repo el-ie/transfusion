eda05fa152f57548a356aca6e07185f481fc3488
Elie AMAR <eamar@made-f0Br7s10.clusters.42paris.fr>
2023-11-01 00:54:08 +0100

    [REACT] [LoginForm] gestion de la route et de l affichage pour la generation du QR code (debut de l authentification 2fa, important : LoginForm n est plus une classe et devient une fonction pour pouvoir utiliser les hooks comme necessaire, les fonctions ont l air plus interessantes a utiliser.

--- requirements/react/app/src/components/LoginForm/LoginForm.jsx
+++ requirements/react/app/src/components/LoginForm/LoginForm.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import axios from 'axios';
 
-export default class LoginForm extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+//export default class LoginForm extends React.Component {
+//	constructor(props) {
+//		super(props);
+//	}
 
-	handleSignup() {
+export default function LoginForm() {
+
+	const [qrCode, setQrCode] = useState (null);
+
+	function handleSignup() {
 		//let page = await axios.get('http://localhost:3001', { withCredentials: true });
 		window.location.href = 'http://localhost:3001/auth/login42';
 	};
 
-	render() {
+	async function handleTwoFa() {
+
+		try {
+			const response = await axios.get('http://localhost:3001/auth/twofa_getqr', { withCredentials: true });
+			setQrCode(response.data);
+		} catch (error) {
+			//pourquoi la requete s envoi en double en cas d erreur, visible dans le terminal web
+			console.log('get qr code fail', error.response);
+		}
+
+	}
+
+	//render(){} 
 		return (
 			<>
 			<br /><br /><br /><br /><br /><br /><br /><br />
 			<div style={{ textAlign: 'center' }}>
 
-			<button onClick={this.handleSignup} style={{ padding: '3px 20px', borderRadius: '4px', position:'relative', left:'-86px' }}>  Signup  </button>
+			<button onClick={handleSignup} style={{ padding: '3px 20px', borderRadius: '4px', position:'relative', left:'-86px' }}>  Signup  </button>
+
+			<button onClick={handleTwoFa} style={{ padding: '3px 20px', borderRadius: '4px', position:'relative', left:'-20px' }}>  2FA  </button>
+
+			<br /><br /><br /><br />
+
+			{qrCode? <img src={qrCode} /> : <p> 2FA inactive </p>}
 
 			</div>
 			</>
 		);
-	}
+
 }
