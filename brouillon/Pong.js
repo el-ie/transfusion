e184988003bad5c972bec0483356d5c4c8870ca6
Elie AMAR <eamar@made-f0Br7s10.clusters.42paris.fr>
2023-11-05 15:10:37 +0100

    [Pong] +++ maths presque fini, angle et vitesse peuvent etre set avec les fonctions

--- brouillon/Pong.js
+++ brouillon/Pong.js
@@ -11,7 +11,7 @@ const Pong = () => {
 	//let ball = { x: 320, y: 240, radius: 10, dx: -3, dy: 0 };
 
 	//epaisseur totale du canva
-	let wwidth = 300;
+	let wwidth = 500;
 	//hauteur totale du canva
 	let hheight = 600;
 	//hauteur paddle
@@ -21,36 +21,73 @@ const Pong = () => {
 	//radius de la balle
 	let ballRadius = 10;
 	//pas mouvement paddle
-	let paddleStep = 3;
+	let paddleStep = 15;
 
 	// vitesse vertivale de la balle
-	let ballDx = 3;
+	let ballDx = -3;
 	//vitesse horizontale
 	let ballDy = 0;
 
+	//ratio de vitesse de la balle, 3 = lent, 7 = plutot rapide
+	let minSpeedBall = 3;
+	let maxSpeedBall = 7;
 
-//setup first angle of the ball
-//useEffect( () => {
-//	changeBallAngle(toRadians(0));
-//}, []);
-
+	//const ballRef = useRef({ x: wwidth / 2 - 100, y: hheight / 2 - 200, radius: 10, dx: ballDx, dy: ballDy });
 	const ballRef = useRef({ x: wwidth / 2, y: hheight / 2, radius: 10, dx: ballDx, dy: ballDy });
+
+	//set angle et vitesse de depart
+	useEffect( () => {
+		//changeBallAngle(toRadians(120));
+		changeBallSpeed(0);
+		console.log('ball speed = ', getBallSpeed());
+
+	}, []);
 
 	const canvasRef = useRef(null);
 
 	//Fonction qui permet de donner un angle donne a la basse sans changer la vitesse
-	const changeBallAngle = (theta) => {
-    const currentDx = ballRef.current.dx;
-    const currentDy = ballRef.current.dy;
-    const currentSpeed = Math.sqrt(currentDx * currentDx + currentDy * currentDy);
+	// attention se base sur un debut d'angle a l'est et non au nord !
+		const changeBallAngle = (theta) => {
+			console.log('entree change ball angle');
+			const currentDx = ballRef.current.dx;
+			const currentDy = ballRef.current.dy;
+			const currentSpeed = Math.sqrt(currentDx * currentDx + currentDy * currentDy);
 
-    ballRef.current.dx = currentSpeed * Math.cos(theta);
-    ballRef.current.dy = currentSpeed * Math.sin(theta);
-}
+			ballRef.current.dx = currentSpeed * Math.cos(theta);
+			ballRef.current.dy = currentSpeed * Math.sin(theta);
+		}
 
-function toRadians(degrees) {
-    return degrees * (Math.PI / 180);
-}
+	function toRadians(degrees) {
+		return degrees * (Math.PI / 180);
+	}
+
+	//newSpeed doit etre entre 0 pour la vitesse la plus basse et 1 pour la plus haute
+	const changeBallSpeed = (newSpeed) => {
+
+		// fine tuning
+		newSpeed = (newSpeed * (maxSpeedBall - minSpeedBall)) + minSpeedBall;
+		if (newSpeed > maxSpeedBall)
+			newSpeed = maxSpeedBall;
+
+		const currentDx = ballRef.current.dx;
+		const currentDy = ballRef.current.dy;
+
+		const currentAngle = Math.atan2(currentDy, currentDx);
+
+		ballRef.current.dx = newSpeed * Math.cos(currentAngle);
+		ballRef.current.dy = newSpeed * Math.sin(currentAngle);
+	}
+
+	const getBallSpeed = () => {
+		const currentDx = ballRef.current.dx;
+		const currentDy = ballRef.current.dy;
+		let rawSpeed = Math.sqrt(currentDx * currentDx + currentDy * currentDy);
+		
+		let speed_finetuned = (rawSpeed - minSpeedBall) / ( maxSpeedBall - minSpeedBall);
+
+		return speed_finetuned;
+	}
+
 
 	let leftPaddle = { x: 35, y: leftPaddleY, width: paddleWidth, height: paddleHeight, dy: 0 };
 
@@ -113,101 +150,111 @@ function toRadians(degrees) {
 
 		let animation_id;
 
+
 		const update = () => {
 
 			const ballX = ballRef.current.x;
 			const ballY = ballRef.current.y;
 
-			let newDx = ballRef.current.dx;
-			let newDy = ballRef.current.dy;
-
 			//// CONTACT AVEC PADDLES /////////////////////////////
 
-			//if (ballRef.current.y + ballRadius <= leftPaddle.y
+				//if (ballRef.current.y + ballRadius <= leftPaddle.y
 
-				// PADDLE GAUCHE //
-				if (ballY - ballRadius <= leftPaddle.y + paddleHeight)
-				if (ballY + ballRadius >= leftPaddle.y)
-				if (ballX - ballRadius < leftPaddle.x + leftPaddle.width)
-				{
-					newDx = -newDx;
-
-					//definir le nouvel angle :
-
-					// le centre vertical du paddle
-					let centre_paddle_Y = leftPaddle.y + (leftPaddle.height / 2);
-					let distance_ball_paddle_Y;
-					//variable  pour savoir si la balle tape au dessus ou dessous du centre du paddle :
-					let quadrant = 1; //pour au dessus
-
-					if (ballY > centre_paddle_Y)
+					// PADDLE GAUCHE //
+					if (ballY - ballRadius <= leftPaddle.y + paddleHeight) //balle au dessus de la partie basse du paddle
+					if (ballY + ballRadius >= leftPaddle.y) //balle au dessous de la partie haute du paddle
+					if (ballX - ballRadius < leftPaddle.x + leftPaddle.width
+						&& ballX + ballRadius > leftPaddle.x) //balle 
 					{
-						distance_ball_paddle_Y = ballY - centre_paddle_Y;
-						quadrant = 0; // la balle tape au dessous du centre
+						//newDx = -newDx;
+						ballRef.current.dx *= -1;
+
+						//definir le nouvel angle :
+
+						// le centre vertical du paddle
+						let centre_paddle_Y = leftPaddle.y + (leftPaddle.height / 2);
+						let distance_ball_paddle_Y;
+						//variable  pour savoir si la balle tape au dessus ou dessous du centre du paddle :
+						let quadrant = 1; //pour au dessus
+
+						if (ballY > centre_paddle_Y)
+						{
+							distance_ball_paddle_Y = ballY - centre_paddle_Y;
+							quadrant = 0; // la balle tape au dessous du centre
+						}
+						else
+							distance_ball_paddle_Y = centre_paddle_Y - ballY;
+
+						// L'angle_impact prendra une valeur de 0 si la balle tape au centre du paddle, jusqu a 1 si la balle tape totalement dans un coin, a partir de cela on va a la fois pouvoir determiner l' angle dans lequel la balle repartira mais aussi son acceleration
+
+						let angle_impact = distance_ball_paddle_Y / (leftPaddle.height / 2);
+
+						angle_impact *= 0.80; //pour ramener le maximum a 1 a la place de 1.2, plus simple 
+						console.log(angle_impact); //enfait angle_impact va jusqu a 1.2
+
+
+						//on envoi une valeur entre 0 et 80 degres ( 0 < angle_impact < 1 )
+						if (angle_impact > 0.3)
+						{
+							if (quadrant === 0)
+								changeBallAngle(toRadians(angle_impact * 70));
+							else
+								changeBallAngle(toRadians(360 - (angle_impact * 70)));
+						}
+						//changeBallAngle(toRadians(80));
+
 					}
-					else
-						distance_ball_paddle_Y = centre_paddle_Y - ballY;
 
-					// L'angle_impact prendra une valeur de 0 si la balle tape au centre du paddle, jusqu a 1 si la balle tape totalement dans un coin, a partir de cela on va a la fois pouvoir determiner l' angle dans lequel la balle repartira mais aussi son acceleration
-					let angle_impact = distance_ball_paddle_Y / (leftPaddle.height / 2);
+					// PADDLE DROIT //
+					if (ballY - ballRadius <= rightPaddle.y + paddleHeight)
+					if (ballY + ballRadius >= rightPaddle.y)
+					if (ballX + ballRadius >= rightPaddle.x)
+					ballRef.current.dx *= -1;
+					//newDx = -newDx;
 
-					console.log(angle_impact);
+					//// CONTACT AVEC BORDS //////////////////////////////
+					//let for_test = wwidth / 2 + 160;
+					let for_test = 0;
 
-					//on envoi une valeur entre 0 et 80 degres ( 0 < angle_impact < 1 )
-					//if (quadrant === 0)
-					changeBallAngle(toRadians(angle_impact * 20));
+					if (ballX > canvas.width - 10 - for_test || ballX <= 10)
+					ballRef.current.dx *= -1;
+					//newDx = -newDx;
 
+					if (ballY > canvas.height - 10 || ballY <= 10)
+					ballRef.current.dy *= -1;
+					//newDy = -newDy;
+					///////////////////////////////////////////////////////
+
+					// ACTUALISATION VALEURS
+					ballRef.current.x += ballRef.current.dx;
+					ballRef.current.y += ballRef.current.dy;
+
+					// Clear canvas
+					context.clearRect(0, 0, canvas.width, canvas.height);
+
+					// Draw Ball
+					context.beginPath();
+					context.arc(ballRef.current.x, ballRef.current.y, ballRef.current.radius, 0, Math.PI * 2);
+					context.fill();
+
+					// Draw Paddles
+					context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+
+					context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+
+					context.fillRect(border_top.x, border_top.y, border_top.width, border_top.height);
+					context.fillRect(border_bot.x, border_bot.y, border_bot.width, border_bot.height);
+					context.fillRect(border_left.x, border_left.y, border_left.width, border_left.height);
+					context.fillRect(border_right.x, border_left.y, border_left.width, border_left.height);
+
+					//aide a la visualisation
+					//context.fillRect(ballX - 250, ballY, 500, 1);
+					//context.fillRect(leftPaddle.x, leftPaddle.y, 20, 1);
+					//context.fillRect(leftPaddle.x, leftPaddle.y + paddleHeight, 20, 1);
+
+					// Next frame
+					animation_id = requestAnimationFrame(update);
 				}
-
-				// PADDLE DROIT //
-				if (ballY - ballRadius <= rightPaddle.y + paddleHeight)
-				if (ballY + ballRadius >= rightPaddle.y)
-				if (ballX + ballRadius >= rightPaddle.x)
-				newDx = -newDx;
-
-				//// CONTACT AVEC BORDS //////////////////////////////
-				//let for_test = wwidth / 2 + 160;
-				let for_test = 0;
-
-				if (ballX > canvas.width - 10 - for_test || ballX <= 10)
-				newDx = -newDx;
-
-				if (ballY > canvas.height - 10 || ballY <= 10)
-				newDy = -newDy;
-				///////////////////////////////////////////////////////
-
-				// ACTUALISATION VALEURS
-				ballRef.current.x += newDx;
-				ballRef.current.y += newDy;
-				ballRef.current.dx = newDx;
-				ballRef.current.dy = newDy;
-
-				// Clear canvas
-				context.clearRect(0, 0, canvas.width, canvas.height);
-
-				// Draw Ball
-				context.beginPath();
-				context.arc(ballRef.current.x, ballRef.current.y, ballRef.current.radius, 0, Math.PI * 2);
-				context.fill();
-
-				// Draw Paddles
-				context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
-
-				context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
-
-				context.fillRect(border_top.x, border_top.y, border_top.width, border_top.height);
-				context.fillRect(border_bot.x, border_bot.y, border_bot.width, border_bot.height);
-				context.fillRect(border_left.x, border_left.y, border_left.width, border_left.height);
-				context.fillRect(border_right.x, border_left.y, border_left.width, border_left.height);
-
-				//aide a la visualisation
-				//context.fillRect(ballX - 250, ballY, 500, 1);
-				//context.fillRect(leftPaddle.x, leftPaddle.y, 20, 1);
-				//context.fillRect(leftPaddle.x, leftPaddle.y + paddleHeight, 20, 1);
-
-				// Next frame
-				animation_id = requestAnimationFrame(update);
-			}
 
 		animation_id = requestAnimationFrame(update);
 
